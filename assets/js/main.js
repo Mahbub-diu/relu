1de42eaff32b813199b3efd102aa8e9c9ac63589
Mahbub-diu <mahbub.mediasoft@gmail.com>
2024-08-30 16:06:40 +0600

    update v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,47 @@
 (function ($) {
   $(document).ready(function () {
     // code goes here
+    var swiper = new Swiper('.home-slider', {
+      effect: 'fade',
+      speed: 2000,
 
+      autoplay: {
+        delay: 5000,
+      },
+      autoplay: true,
+
+      loop: true,
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+    $('.swiper-video').on('loadeddata', function () {
+      var duration = this.duration;
+
+      var autoplayDelay = Math.round(duration * 1000);
+
+      $(this)
+        .closest('.video-slide')
+        .attr('data-swiper-autoplay', autoplayDelay);
+
+      console.log('Video Duration: ' + duration + ' seconds');
+    });
+
+    var swiper = new Swiper('.service-slider', {
+      effect: 'fade',
+      speed: 2000,
+
+      autoplay: {
+        delay: 5000,
+      },
+      autoplay: true,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
     $(window).scroll(function () {
       var navbar = $('#main-header');
 
